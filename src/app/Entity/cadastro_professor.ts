801b543c3d0fcd8193066eb9abb54f1afaee3e33
LucDutra <fucagui@gmail.com>
2021-10-17 18:16:02 -0300

    Feito html e css do cadastro do professor

---
+++ src/app/Entity/cadastro_professor.ts
@@ -0,0 +1,11 @@
+
+export class Escola{
+    id?: number;
+    nome?: String;
+    graduacao?: number;
+    email?: String;
+    telefone?: String;
+    cpf?: String;
+    estadoCivil?: String;
+
+}
---
+++ src/app/cadastro_professor/cadastro_professor.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro_professor',
+  templateUrl: './cadastro_professor.html',
+  styleUrls: ['./cadastro_professor.component.css']
+})
+export class cadastro_professorComponent implements OnInit {
+
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
