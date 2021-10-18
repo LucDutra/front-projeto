74ed1183bca581945f38352de9aa6aa97fe046c7
firmo10 <firmoguilhevieira2015@gmail.com>
2021-10-18 20:58:49 -0300

    entity perfilEscola

---
+++ src/app/entitys/perfilEscola.ts
@@ -0,0 +1,10 @@
+export class perfilEscola{
+    endereco?: String;
+    uf?: String;
+    cidade?: String;
+    telefone?: String;
+    cnpj?: String;
+    email?: String;
+    matricula?: number;
+   
+}
