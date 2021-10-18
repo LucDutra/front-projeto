b0dff9ec7a63590d91d381234261cbeaa594a7a7
firmo10 <firmoguilhevieira2015@gmail.com>
2021-10-18 20:41:34 -0300

    entity cadastroProfessor

---
+++ src/app/entitys/cadastroProfessor.ts
@@ -0,0 +1,9 @@
+export class cadastroProfessor{
+    nome?: String;
+    email?: String;
+    telefone?: String;
+    cpf?: String;
+    estadoCivil?: String;
+    graduacao?: String;
+    formação?: String;
+}
