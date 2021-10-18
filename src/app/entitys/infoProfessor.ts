fa3a5692882b26d1e3fda8f93be5f653c327a1a2
firmo10 <firmoguilhevieira2015@gmail.com>
2021-10-18 20:47:35 -0300

    entity infoProfessor

---
+++ src/app/entitys/infoProfessor.ts
@@ -0,0 +1,10 @@
+export class cadastroAluno{
+    nome?: number;
+    email?: String;
+    telefone?: String;
+    cpf?: String;
+    materias?: String;
+    turmas?: String;
+    formacao?: String;
+    descricao?: String;
+}
