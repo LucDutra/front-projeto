725c7770b3a001550c399f995fee918311a8c0a1
firmo10 <firmoguilhevieira2015@gmail.com>
2021-10-18 20:35:53 -0300

    entity cadastroAluno

---
+++ src/app/entitys/cadastroAluno.ts
@@ -0,0 +1,20 @@
+export class cadastroAluno{
+    id?: number;
+    escola?: String;
+    cpf?: String;
+    telefone?: String;
+    turma?: String;
+    email?: String;
+    sexo?: String;
+    nomeResponsavel?: String;
+    endereco?: String;
+    telefoneResponsavel?: String;
+    cpfResponsavel?: String;
+    estavoCivil?: String;
+    emailRsponsavel?: String;
+    tipoSanguineo?: String;
+    deficiencia?: String;
+    peso?: String;
+    altura?: String;
+    planoDeSaude?: String; 
+}
