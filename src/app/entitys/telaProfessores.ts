6570fc01d40b120c80eea4454dc87f8f9865777b
firmo10 <firmoguilhevieira2015@gmail.com>
2021-10-18 21:07:51 -0300

    entity telaProfessores

---
+++ src/app/entitys/telaProfessores.ts
@@ -0,0 +1,10 @@
+export class telaProfessores{
+    turma?: String;
+    lpo?: String;
+    mat?: String;
+    qui?: String;
+    fis?: String;
+    edf?: String;
+    bio?: String;
+    hist?: String;
+}
